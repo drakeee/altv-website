f7b1dd46fe9de5f76c9ae52166da9be51aad0114
drakeee <drakedrift@gmail.com>
2021-01-03 14:48:21 +0100

    Init.

---
+++ precache-manifest.3822cb7b97a7e0728796d9a53b57715d.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "518372e3df795af7e0179d4743006d98",
+    "url": "/branding/png/logo.png"
+  },
+  {
+    "revision": "23c67ea7718fb9cdf87b977dcf89dcfb",
+    "url": "/branding/png/logo_black.png"
+  },
+  {
+    "revision": "0b857de2ce12c1f14a2f2b588c1f9a70",
+    "url": "/branding/svg/logo.svg"
+  },
+  {
+    "revision": "6f950a3b3816ac57ccc2ad394b929f22",
+    "url": "/branding/svg/logo_black.svg"
+  },
+  {
+    "revision": "62f273b6b30c88ca2d22",
+    "url": "/css/app.0df65eda.css"
+  },
+  {
+    "revision": "b5dc0c807c348e87ddb0a3a0db163d39",
+    "url": "/fonts/Inter-Black.woff2"
+  },
+  {
+    "revision": "0564381b22b2c4760eb15bef0691993a",
+    "url": "/fonts/Inter-Bold.woff2"
+  },
+  {
+    "revision": "000899d460b7eb798c2a9363b6bc5bae",
+    "url": "/fonts/Inter-Medium.woff2"
+  },
+  {
+    "revision": "d55e957612a3b2a373925d920f4af6b1",
+    "url": "/fonts/Inter-Regular.woff2"
+  },
+  {
+    "revision": "dbe8505cf4eb137c63b6c375e02c225e",
+    "url": "/fonts/fa.dbe8505c.woff2"
+  },
+  {
+    "revision": "9c4bddbcc23b6c5bd38c03eadb343510",
+    "url": "/fonts/fonts.css"
+  },
+  {
+    "revision": "77812e2fead777db0dc806b1eaabc7fd",
+    "url": "/fonts/i400.77812e2f.woff2"
+  },
+  {
+    "revision": "900f31022873fa16552980d331bebfcc",
+    "url": "/fonts/i500.900f3102.woff2"
+  },
+  {
+    "revision": "3d011804596fb2ccde4af51830a9a7d5",
+    "url": "/fonts/i600.3d011804.woff2"
+  },
+  {
+    "revision": "741dc4163a97ffa354979f18802f6ca8",
+    "url": "/fonts/i700.741dc416.woff2"
+  },
+  {
+    "revision": "3f65bd10dd37478e619a59dfd8014224",
+    "url": "/fonts/i800.3f65bd10.woff2"
+  },
+  {
+    "revision": "9937c52ecf1224033b83886cbfb10ae6",
+    "url": "/fonts/u400.9937c52e.woff2"
+  },
+  {
+    "revision": "0eb43950c0afd2e9c7a553916979d44c",
+    "url": "/img/android-chrome-192x192.png"
+  },
+  {
+    "revision": "f4fcf31866cc2ca9beb59937a9bab4a5",
+    "url": "/img/android-chrome-512x512.png"
+  },
+  {
+    "revision": "308b7ac23ca4f0ea0ebc3e4737e87fc2",
+    "url": "/img/apple-touch-icon.png"
+  },
+  {
+    "revision": "1268d62960ba5a2d5209dbbdc3834cc5",
+    "url": "/img/back.1268d629.png"
+  },
+  {
+    "revision": "08cc3e3f7e1e8aac906a1667c74e16a3",
+    "url": "/img/browserconfig.xml"
+  },
+  {
+    "revision": "61202726bc8d16eb85d99778b7529219",
+    "url": "/img/favicon-16x16.png"
+  },
+  {
+    "revision": "1882eb69673e4f2db2206a782126b11a",
+    "url": "/img/favicon-32x32.png"
+  },
+  {
+    "revision": "5139557fd55e1a4fc513bf92e0da9aa1",
+    "url": "/img/mstile-150x150.png"
+  },
+  {
+    "revision": "44bfc041a94311edc4b64864a06b6b87",
+    "url": "/img/og-image.png"
+  },
+  {
+    "revision": "fbd742a91c18b663ca4a8a6712317971",
+    "url": "/img/powerhost.fbd742a9.svg"
+  },
+  {
+    "revision": "c08c4734c038fdc4493af5fa3dcdce63",
+    "url": "/img/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "a242bf3b3e3d8a375b992edf70365258",
+    "url": "/img/site.webmanifest"
+  },
+  {
+    "revision": "0b857de2ce12c1f14a2f2b588c1f9a70",
+    "url": "/img/v_logo.0b857de2.svg"
+  },
+  {
+    "revision": "0b857de2ce12c1f14a2f2b588c1f9a70",
+    "url": "/img/v_logo.svg"
+  },
+  {
+    "revision": "10146ae7f34570f01c22ce35673f92fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62f273b6b30c88ca2d22",
+    "url": "/js/app.06e3a86b.js"
+  },
+  {
+    "revision": "3a547fa4d7b6b2cb28db",
+    "url": "/js/chunk-vendors.9dbd710b.js"
+  },
+  {
+    "revision": "e148a5134ea15f58bd898a9b7284ca8f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "485f126a6ee1c9ce1ff99a1fcaa9b80b",
+    "url": "/terms-of-service.pdf"
+  }
+]);
